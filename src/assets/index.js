1117f2007b801b39866b04281e76f839cef9a6f5
Alaaeldin Abdelaal <AlaaeldinAbdelaal94@gmail.com>
2025-11-20 23:57:44 +0000

    PP-001: init version
    The first version of the profile website

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+// Import company logos
+import alten from "./company/alten.png";
+import uclan from "./company/uclan.png";
+import ge from "./company/ge.png";
+
+// Import tech stack images
+import python from "./tech-stack/python.png";
+import c from "./tech-stack/c.png";
+import cpp from "./tech-stack/cpp.png";
+import typescript from "./tech-stack/typescript.png";
+import go from "./tech-stack/go.png";
+
+// Import project images
+import photopartpro from "./projects/photopartpro.gif";
+import sms from "./projects/sms.jpg";
+import payloadmaster from "./projects/payloadmaster.png";
+import dp from "./projects/dp.jpg";
+import sketcher from "./projects/sketcher.png";
+import CompileVortex from "./projects/CompileVortex.png";
+
+// Import tech logos
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import angular from "./tech/angular.png";
+
+// Import common assets
+import logo from "./logo.svg";
+import link from "./link.jpg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+export {
+  alten,
+  uclan,
+  python,
+  c,
+  cpp,
+  go,
+  link,
+  css,
+  git,
+  html,
+  javascript,
+  typescript,
+  nodejs,
+  reactjs,
+  angular,
+  photopartpro,
+  sms,
+  payloadmaster,
+  dp,
+  sketcher,
+  CompileVortex,
+  logo,
+  menu,
+  close,
+  ge,
+};
