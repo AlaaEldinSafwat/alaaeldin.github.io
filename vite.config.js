15cc442396a361f788a748c2897ba0c0d13d235d
Alaaeldin Abdelaal <AlaaeldinAbdelaal94@gmail.com>
2025-11-21 01:07:39 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/alaaeldinsafwat.github.io/alaaeldin.github.io"
+  base: '/alaaeldin.github.io/'
 });
